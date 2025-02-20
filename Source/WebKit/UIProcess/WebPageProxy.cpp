3ef218ef6d53def90c70f09028c62dc88833a59a
Vitaly Dyachkov <vitaly@igalia.com>
2025-02-20 10:07:37 +0100

    Resolve conflicts

--- Source/WebKit/UIProcess/WebPageProxy.cpp
+++ Source/WebKit/UIProcess/WebPageProxy.cpp
@@ -8694,13 +8694,9 @@ void WebPageProxy::createNewPage(IPC::Connection& connection, WindowFeatures&& w
     configuration->setInitialSandboxFlags(effectiveSandboxFlags);
     configuration->setWindowFeatures(WTFMove(windowFeatures));
     configuration->setOpenedMainFrameName(openedMainFrameName);
-<<<<<<< HEAD
     if (!protectedPreferences()->siteIsolationEnabled())
         configuration->setRelatedPage(*this);
-||||||| parent of c1194e7ba0da (chore(webkit): bootstrap build #2140)
-=======
     configuration->setOpenerPageForInspector(*this);
->>>>>>> c1194e7ba0da (chore(webkit): bootstrap build #2140)
 
     if (RefPtr openerFrame = WebFrameProxy::webFrame(originatingFrameInfoData.frameID); navigationActionData.hasOpener && openerFrame) {
         configuration->setOpenerInfo({ {
