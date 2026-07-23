f30d353536307615bc92d96debf7c92730da7ea4
Vitaly Dyachkov <vitaly@igalia.com>
2026-07-23 14:33:51 +0000

    cURL: Don't add default  if the user has explicitly set one

--- Source/WebCore/platform/network/curl/CurlRequest.cpp
+++ Source/WebCore/platform/network/curl/CurlRequest.cpp
@@ -544,6 +544,10 @@ int CurlRequest::didReceiveDebugInfo(curl_infotype type, std::span<const char> d
 
 void CurlRequest::appendAcceptLanguageHeader(HTTPHeaderMap& header)
 {
+    // Don't add default Accept-Language if the user has explicitly set one
+    if (header.contains(HTTPHeaderName::AcceptLanguage))
+        return;
+
     for (const auto& language : userPreferredLanguages())
         header.add(HTTPHeaderName::AcceptLanguage, language);
 }
