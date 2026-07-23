42f17155793448781d13fc564b3e11435c1c281c
Vitaly Dyachkov <vitaly@igalia.com>
2026-07-23 11:49:13 +0000

    Fix Windows build after 317567@main

--- Source/ThirdParty/skia/include/core/SkStrikeRef.h
+++ Source/ThirdParty/skia/include/core/SkStrikeRef.h
@@ -71,7 +71,9 @@ public:
 
 private:
     friend class SkFont;
-    SkStrikeRef(sk_sp<SkStrike> strike, SkScalar strikeToSourceScale);
+    SkStrikeRef(sk_sp<SkStrike> strike, SkScalar strikeToSourceScale)
+        : fStrike(std::move(strike))
+        , fStrikeToSourceScale(strikeToSourceScale) {}
 
     sk_sp<SkStrike> fStrike;
     SkScalar fStrikeToSourceScale = 0;
