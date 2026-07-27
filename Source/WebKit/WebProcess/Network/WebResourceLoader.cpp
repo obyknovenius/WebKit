44c4a54dc1907d139b1f96fe5b7e18a23a5f19bf
playwright-devops <devops@playwright.dev>
2026-07-27 07:05:16 +0000

    chore(webkit): bootstrap build #2339

--- Source/WebKit/WebProcess/Network/WebResourceLoader.cpp
+++ Source/WebKit/WebProcess/Network/WebResourceLoader.cpp
@@ -275,9 +275,6 @@ void WebResourceLoader::didReceiveResponse(ResourceResponse&& response, PrivateR
 
             coreLoader->didReceiveResponse(ResourceResponse { inspectorResponse }, [this, protectedThis = Ref { *this }, interceptedRequestIdentifier, policyDecisionCompletionHandler = WTF::move(policyDecisionCompletionHandler), overrideData = WTF::move(overrideData)]() mutable {
                 RefPtr coreLoader = m_coreLoader;
-                if (policyDecisionCompletionHandler)
-                    policyDecisionCompletionHandler();
-
                 if (!m_coreLoader || !coreLoader->identifier()) {
                     m_interceptController.continueResponse(interceptedRequestIdentifier);
                     return;
@@ -294,6 +291,8 @@ void WebResourceLoader::didReceiveResponse(ResourceResponse&& response, PrivateR
                 }
             });
         });
+        if (policyDecisionCompletionHandler)
+          policyDecisionCompletionHandler();
         return;
     }
 
